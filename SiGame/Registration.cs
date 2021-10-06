2693c697a7846d085cbbcc7d8d711194aba0211b
IsypMykhailo <amazonkanameforme@gmail.com>
2021-10-06 21:03:57 +0300

    database

--- SiGame/Registration.cs
+++ SiGame/Registration.cs
@@ -7,26 +7,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Entity;
 
 namespace SiGame
 {
     public partial class Registration : Form
     {
+        DBSiGameEntities db;
+        //bool UserAdded;
         public Registration()
         {
             InitializeComponent();
+            //UserAdded = false;
+            db = new DBSiGameEntities(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\ПРОГРАММИРОВАНИЕ\Программирование\Курсовая\SiGame\SiGame\DBSiGame.mdf;Integrated Security=True");
         }
 
         private void btnSignUp_Click(object sender, EventArgs e)
-        {
+        {            
             if (IsValidEmail(txbEmail.Text))
             {
-                this.Close();
+                if (txbPassword.Text == txbConfirmPassword.Text)
+                {
+                    Users user = new Users()
+                    {
+                        Id = Guid.NewGuid(),
+                        FirstName = txbFirstName.Text,
+                        LastName = txbLastName.Text,
+                        Email = txbEmail.Text,
+                        Username = txbUsername.Text,
+                        Password = txbPassword.Text
+                    };
+
+                    var checkUsername = (from u in db.Users
+                                where txbUsername.Text == u.Username
+                                select u).ToList();
+                    var checkEmail = (from u in db.Users
+                                      where txbEmail.Text == u.Email
+                                      select u).ToList();
+                    if (checkUsername.Count == 0 && checkEmail.Count == 0)
+                    {
+                        db.Users.Add(user);
+                        db.SaveChanges();
+                        this.Close();
+                    }
+                    else if (checkUsername.Count != 0 && checkEmail.Count == 0)
+                    {
+                        MessageBox.Show("Such Username exists", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else if(checkEmail.Count != 0 && checkUsername.Count == 0)
+                    {
+                        MessageBox.Show("Such Email exists", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Such Username and Email exists", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
             }
             else
             {
                 MessageBox.Show("Incorrect Email", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            }           
+            
         }
         bool IsValidEmail(string email)
         {
